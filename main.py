6fb3191df425c3bba14dcca42a02ca81453bbca3
RICHARD FRANCO <33595611+dimurano@users.noreply.github.com>
2026-07-28 18:35:55 -0400

    Remove Python version info from main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-Python 3.14.6 (v3.14.6:c63aec69bd5, Jun 10 2026, 08:07:54) [Clang 21.0.0 (clang-2100.1.1.101)] on darwin
 Enter "help" below or click "Help" above for more information.
 >>> import logging
 ... import os
